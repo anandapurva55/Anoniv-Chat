8ef9a5f274d577b9b1dd82a43de3f9625b6c20af
anandapurva55 <31860248+anandapurva55@users.noreply.github.com>
2020-02-28 00:20:10 +0530

    Create Messages.swift

---
+++ Model/Messages.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Anoniv Chat iOS13
+//
+//  Created by apurva anand on 2/1/20.
+//  Copyright © 2020 Apurva Anand. All rights reserved.
+//
+
+import Foundation
+struct Message {
+    let sender: String
+    let body: String
+}
